372f7efd1a9372d5089034d92a4331fc83fc4893
Nicolas Parada <nicolas.pc.1700@gmail.com>
2022-08-30 09:02:53 -0500

    Se agrega lo aprendido en property binding, property binding con objetos y event binfing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,24 @@ export class AppComponent {
   title = 'Aprendiendo Angular';
   name = 'Nicolas';
   age = 18;
-  img = 'https://http2.mlstatic.com/D_NQ_NP_740980-MCO31567535004_072019-O.jpg'
+  img = 'https://http2.mlstatic.com/D_NQ_NP_740980-MCO31567535004_072019-O.jpg';
+  bntDisable = true;
+  valueInput = 'Nicolas lo hizo :) ';
+  Person = {
+    name: 'Nicolas',
+    age: 18,
+    avatar: 'https://http2.mlstatic.com/D_NQ_NP_740980-MCO31567535004_072019-O.jpg'
+  }
+  toggleButton () {
+    /* Se niega el estado actual del boton,
+    para obtener true o false segun el estado del boton
+    */
+    this.bntDisable = !this.bntDisable;
+  }
+  ageMore(){
+    this.Person.age += 1;
+  }
+  ageLees(){
+    this.Person.age -= 1;
+  }
 }
